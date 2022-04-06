4e37660a8b1804fcfe21382507a3f49417499257
Nagy <janos@zold.lan>
2022-04-06 12:15:27 +0200

    Kezdés

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Tama verzió: 0.0.1");
+        Tama tama = new Tama("Tama");
+        tama.ertelmezo();
+    }
+}
---
+++ src/Tama.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Tama extends Leny {
+    public String nev;
+    public int ehseg = -5;
+    public Tama(String nev) {
+        this.nev = nev;
+    }
+
+    @Override
+    public void ertelmezo() {
+        Scanner sc = new Scanner(System.in);
+        
+        String parancs = "";
+        while(!parancs.equals("vege")) {
+            System.out.print(this.nev + "> ");
+            parancs = sc.nextLine();
+            eheztetes();
+        }
+
+    }
+    private void eheztetes() {
+        //éheztetés
+        this.ehseg++;
+        if(this.ehseg > 0) {
+            System.out.println("Éhes vagyok!");
+        }
+    }
+}
